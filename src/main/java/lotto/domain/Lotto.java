8b91079559bc854b6c282f35ea0a4e9616af0452
qkdrmawll <eunji8106@naver.com>
2023-11-14 03:06:14 +0900

    feat : Lotto toString 오버라이딩 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // TODO: 추가 기능 구현
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (Integer i:numbers) {
+            stringBuilder.append(i).append(", ");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}
