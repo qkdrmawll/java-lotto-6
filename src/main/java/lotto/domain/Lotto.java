7e7bb14a12c964306e31853f4094fb9c88f2e14f
qkdrmawll <eunji8106@naver.com>
2023-11-17 20:37:30 +0900

    feat : 로또 번호 정렬, 중복 검사 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,22 +1,54 @@
 package lotto.domain;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sort(numbers);
+    }
+
+    private List<Integer> sort(List<Integer> numbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (isDuplicate(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isDuplicate(List<Integer> numbers) {
+        HashSet<Integer> set = new HashSet<>(numbers);
+        if (set.size()!=6) {
+            return true;
+        }
+        return false;
     }
 
     // TODO: 추가 기능 구현
+
+    public int checkNumbersMatch(Lotto lotto) {
+        List<Integer> otherNumbers = lotto.numbers;
+        int matchNum = 0;
+        for (Integer i : otherNumbers) {
+            if (isContainNumber(i)){
+                matchNum++;
+            }
+        }
+        return matchNum;
+    }
+
+    public boolean isContainNumber(Integer i) {
+        return numbers.contains(i);
+    }
 
     @Override
     public String toString() {
