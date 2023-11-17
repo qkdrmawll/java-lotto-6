f7a89040a7a0b9fab5d17daccce1e3d039328b63
qkdrmawll <eunji8106@naver.com>
2023-11-17 20:40:10 +0900

    design : 결과 출력 수정

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -20,8 +20,11 @@ public class LottoView {
         for (Lotto lotto : lottoList) {
             System.out.println(lotto);
         }
+        System.out.println();
     }
     public void printWinningDetails(int[] winningDetails) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
         System.out.println("3개 일치 (5,000원) - " + winningDetails[0] + "개");
         System.out.println("4개 일치 (50,000원) - " + winningDetails[1] + "개");
         System.out.println("5개 일치 (1,500,000원) - " + winningDetails[2] + "개");
