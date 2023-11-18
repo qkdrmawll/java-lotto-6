a26524eb1675a86967b435a7fbfd821f0b0abb45
qkdrmawll <eunji8106@naver.com>
2023-11-18 18:34:48 +0900

    feat : InputView 출력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+public class InputView {
+    public void inputPurchasePrice() {
+        System.out.println("구매금액을 입력해 주세요.");
+    }
+    public void inputWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+    public void inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
